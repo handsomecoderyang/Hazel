ce897af95871e6e880ee27b88cd95c489a9a0b45
杨俊生 <yxf1272455060@163.com>
2023-06-03 21:22:45 +0800

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,19 @@
+#include "Application.h"
+
+
+namespace Hazel{
+	Hazel::Application::Application()
+	{
+	}
+
+	Hazel::Application::~Application()
+	{
+	}
+	void Application::Run()
+	{
+		while (true)
+		{
+
+		}
+	}
+}
